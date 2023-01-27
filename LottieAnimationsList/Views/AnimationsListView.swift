b5f7223c75b6169276b4d2bcbac586e3016f0304
Ruiyan Liang <Jliang@lcs.on.ca>
2023-01-27 14:20:33 -0500

    Organized project and created a list view to show interesting animations.

---
+++ LottieAnimationsList/Views/AnimationsListView.swift
@@ -0,0 +1,20 @@
+//
+//  AnimationsListView.swift
+//  LottieAnimationsList
+//
+//  Created by dyhidrogen monoxide on 2023/1/27.
+//
+
+import SwiftUI
+
+struct AnimationsListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AnimationsListView_Previews: PreviewProvider {
+    static var previews: some View {
+        AnimationsListView()
+    }
+}
