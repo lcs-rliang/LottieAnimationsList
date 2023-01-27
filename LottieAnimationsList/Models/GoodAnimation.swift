348b7769db997326f20983571a1b807f34899002
Ruiyan Liang <Jliang@lcs.on.ca>
2023-01-27 15:11:50 -0500

    Added a structure and an array to describe some good animations.

---
+++ LottieAnimationsList/Models/GoodAnimation.swift
@@ -0,0 +1,38 @@
+//
+//  GoodAnimation.swift
+//  LottieAnimationsList
+//
+//  Created by dyhidrogen monoxide on 2023/1/27.
+//
+
+import Foundation
+
+struct GoodAnimation: Identifiable {
+    
+    let id = UUID()
+    let fileName: String
+    let description: String
+    
+}
+
+// Create a list of animations that are favourites
+let favourites = [
+    
+    GoodAnimation(fileName: "Loding Cat",
+                  description: "Loding Cat")
+    
+    ,
+    
+    GoodAnimation(fileName: "118267-guitar-to-axe-morphing",
+                  description: "Guitar To Axe")
+    
+    ,
+    
+    GoodAnimation(fileName: "125659-melting",
+                  description: "Melting")
+                  
+    ,
+                  
+    GoodAnimation(fileName: "82652-ghosts",
+                  description: "Looping Ghosts")]
+                  
