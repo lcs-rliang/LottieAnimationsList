b4dacc6b6ff2078d870e8d3af62b9366e10790f1
Ruiyan Liang <Jliang@lcs.on.ca>
2023-01-27 14:15:19 -0500

    Initial Commit

---
+++ LottieAnimationsList/LottieAnimationsListApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottieAnimationsListApp.swift
+//  LottieAnimationsList
+//
+//  Created by dyhidrogen monoxide on 2023/1/27.
+//
+
+import SwiftUI
+
+@main
+struct LottieAnimationsListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
